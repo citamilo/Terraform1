f765964a7e4136474a2ab92c9242b7f40f07370c
Daniel Augusto Sánchez <ninj68@hotmail.com>
2023-02-20 13:06:39 -0500

    nueva cola

--- queues.tf
+++ queues.tf
@@ -65,3 +65,20 @@ resource "genesyscloud_routing_queue" "Brokerage" {
   #   ring_num = 2
   # }
 }
+
+resource "genesyscloud_routing_queue" "Brokerage1" {
+  name                              = "Brokerage1"
+  description                       = "Brokerage team1"
+  acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
+  acw_timeout_ms                    = 300000
+  skill_evaluation_method           = "BEST"
+  auto_answer_only                  = true
+  enable_transcription              = true
+  enable_manual_assignment          = true
+  calling_party_name                = "Example Inc."
+
+  # members {
+  #   user_id  = genesyscloud_user.test_user.id
+  #   ring_num = 2
+  # }
+}
