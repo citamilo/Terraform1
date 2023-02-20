363adb3b48db0a31ad52a0a036af45cb25236770
Daniel Sanchez <ninj68@hotmail.com>
2023-02-20 08:11:43 -0500

    actualización nombre

--- queues.tf
+++ queues.tf
@@ -1,4 +1,4 @@
-resource "genesyscloud_routing_queue" "IRA" {
+resource "genesyscloud_routing_queue" "IRA3" {
   name                              = "IRA"
   description                       = "Individual Retirement Accounts team"
   acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
