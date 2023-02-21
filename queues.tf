d6788db855a166e49501a76ed02db3a30e4e98a5
Daniel Augusto Sánchez <ninj68@hotmail.com>
2023-02-20 20:12:05 -0500

    añadir tfstate

--- queues.tf
+++ queues.tf
@@ -82,3 +82,20 @@ resource "genesyscloud_routing_queue" "Brokerage1" {
   #   ring_num = 2
   # }
 }
+
+resource "genesyscloud_routing_queue" "Brokerage2" {
+  name                              = "Brokerage2"
+  description                       = "Brokerage team2"
+  acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
+  acw_timeout_ms                    = 300000
+  skill_evaluation_method           = "BEST"
+  auto_answer_only                  = true
+  enable_transcription              = true
+  enable_manual_assignment          = true
+  calling_party_name                = "Example Inc."
+
+  # members {
+  #   user_id  = genesyscloud_user.test_user.id
+  #   ring_num = 2
+  # }
+}
