ec2123ad4c774a362f44ec47175cf6875d874e06
Prueba Prueba <Prueba@clarocoe.onmicrosoft.com>
2023-02-24 11:47:50 -0500

    agregar cola

--- queues.tf
+++ queues.tf
@@ -77,3 +77,24 @@ resource "genesyscloud_routing_queue" "Brokerage" {
   #   ring_num = 2
   # }
 }
+
+
+resource "genesyscloud_routing_queue" "Brokerage2" {
+  name                              = "Brokerage2"
+  division_id                       = genesyscloud_auth_division.Terraform_Pruebas.id
+  description                       = "Brokerage team2"
+  acw_wrapup_prompt                 = "MANDATORY_TIMEOUT"
+  acw_timeout_ms                    = 300000
+  skill_evaluation_method           = "BEST"
+  auto_answer_only                  = true
+  enable_transcription              = true
+  enable_manual_assignment          = true
+  calling_party_name                = "Example Inc."
+  depends_on = [
+    genesyscloud_auth_division.Terraform_Pruebas
+  ]
+  # members {
+  #   user_id  = genesyscloud_user.test_user.id
+  #   ring_num = 2
+  # }
+}
