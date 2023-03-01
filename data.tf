4be20431217aa4def4a2498cd5e2fc7df5daab1d
Prueba Prueba <Prueba@clarocoe.onmicrosoft.com>
2023-03-01 18:31:03 -0500

    borrar todo

---
+++ data.tf
@@ -0,0 +1,7 @@
+# data "genesyscloud_telephony_providers_edges_site" "site" {
+#   name = "Laboratorio Piso 10"
+# }
+
+# output "foo" {
+#   value=data.genesyscloud_telephony_providers_edges_site.site
+# }
